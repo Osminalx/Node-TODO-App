7d9ddce9603d20592c0ca28c4c5afa60dc5f01d7
Osminalx <verdestro64@gmail.com>
2025-03-17 22:21:08 -0700

    :sparkles: cookies & JWT config init

--- src/Controllers/UserController.ts
+++ src/Controllers/UserController.ts
@@ -1,5 +1,7 @@
 import {createUser, login} from '../Services/UserService.ts'
 import type { Request, Response } from 'express';
+import jwt from 'jsonwebtoken'
+import { SECRET_JWT_KEY } from '../config.ts';
 
 export const createUserController = async (req: Request, res: Response) => {
     try {
@@ -17,7 +19,19 @@ export const createUserController = async (req: Request, res: Response) => {
 export const loginController = async (req: Request, res: Response) => {
     try {
         const user = await login(req.body)
-        res.status(200).json(user)
+        const token = jwt.sign({userId: user.id, role: user.role,email: user.email}, 
+            SECRET_JWT_KEY,
+            {
+            expiresIn: '1h'
+        })
+        res
+            .cookie('access_token',token,{
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60
+            })
+        .send({user,token})
     } catch (error) {
         if(error instanceof Error){
         res.status(401).send(error.message)
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const{
     PORT = 3000,
     SALT_ROUNDS = 10,
+    SECRET_JWT_KEY = 'this-is-a-awesome-secret-key-mucho-mas-largo-significa-mas-seguro-y-con-hash-mucho-mejor',
 } = process.env;
